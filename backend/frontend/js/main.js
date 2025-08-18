dae730c77c69843f6793f635dc205fefdc397378
fomnanpan12 <fomnanpan12@gmail.com>
2025-08-18 14:31:24 +0100

    demo ready

--- backend/frontend/js/main.js
+++ backend/frontend/js/main.js
@@ -1,4 +1,4 @@
-const API = localStorage.getItem("API") || "https://hakaton-1lu4.onrender.com/";
+const API = "https://hakaton-1lu4.onrender.com/";
 
 // ====================== QR SCANNER PAGE ======================
 
